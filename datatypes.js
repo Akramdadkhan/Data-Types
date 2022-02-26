2923ed300e1e34ecfbe1e5491990a1d344e5e0d6
Akramdadkhan <97161831+Akramdadkhan@users.noreply.github.com>
2022-02-26 21:39:53 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,54 @@
+// let i=0;
+
+// while(i < 10){
+//     i++;
+//     console.log(i); 
+// }
+
+// do while
+
+// let b = 31;
+
+// do  {
+    
+//     console.log(a);
+//     a++
+// } while (a<30);
+
+// const person = {
+//     name: "Akram",
+//     age: 19,
+//     city: "Nanded",
+
+
+// };
+
+// for(key in person){
+//         console.log(person[key]);
+// }
+
+// const arr = [1,2,3,4,5,"abc",7,8,9];
+
+// for(g of arr){
+//     console.log(g );
+// }
+
+// let role = "Guest";
+
+// switch(role){
+//         case "Admin":
+//         console.log("Hello Admin");
+//         break;
+//         case "user":
+//         console.log("Hello Users");
+//         break;
+//         case "Guest":
+//         console.log("Hello Guest");
+//         break;
+//         default:
+//             console.log("You are not authorized")
+
+
+
+// }
+
